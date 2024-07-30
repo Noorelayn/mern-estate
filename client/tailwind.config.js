b2c413999141e53b4b0081ba679f802a6c6a3781
noor <nurayneabdulsemed@gmail.com>
2024-07-30 10:10:01 +0200

    first commit

---
+++ client/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,js,ts,jsx,tsx}", // Add your source files here
+    "./public/index.html", // Add the path to your main HTML file(s)
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
